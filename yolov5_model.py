3bd8ae092e52051d5f4cec6ab5ff8d58581d7f8d
lixiangfu <xiangfuli96@gmail.com>
2021-11-28 00:45:56 -0500

    add yolov5 model package to lib path

--- yolov5_model.py
+++ yolov5_model.py
@@ -1,11 +1,17 @@
 import torch
+import os
+import sys
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = BASE_DIR
+sys.path.append(BASE_DIR)
+sys.path.append(os.path.join(ROOT_DIR, 'models/yolov5'))
 
-from model.yolov5.models.common import DetectMultiBackend
-from model.yolov5.utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
-from model.yolov5.utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr,
+from models.common import DetectMultiBackend
+from utils.datasets import IMG_FORMATS, VID_FORMATS, LoadImages, LoadStreams
+from utils.general import (LOGGER, check_file, check_img_size, check_imshow, check_requirements, colorstr,
                            increment_path, non_max_suppression, print_args, scale_coords, strip_optimizer, xyxy2xywh)
-from model.yolov5.utils.plots import Annotator, colors, save_one_box
-from model.yolov5.utils.torch_utils import select_device, time_sync
+from utils.plots import Annotator, colors, save_one_box
+from utils.torch_utils import select_device, time_sync
 
 class Yolov5Model:
     def __init__(self, 
