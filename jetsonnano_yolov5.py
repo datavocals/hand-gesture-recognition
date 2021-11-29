1038cb36cfc9cb5190266d6b92fba9f52fb4f1ca
lixiangfu <xiangfuli96@gmail.com>
2021-11-29 08:20:26 -0500

    send msg to arduino in jetson

--- jetsonnano_yolov5.py
+++ jetsonnano_yolov5.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import sys
+import subprocess
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = BASE_DIR
 sys.path.append(BASE_DIR)
@@ -8,6 +9,11 @@ sys.path.append(os.path.join(ROOT_DIR, 'utils'))
 sys.path.append(os.path.join(ROOT_DIR, 'model/yolov5'))
 from yolov5_model import Yolov5Model
 from capture_frame_in_csi_camera import gstreamer_pipeline
+
+# set speed when communicate with arduino
+subprocess.Popen(["stty", "9600", "-F",  "/dev/ttyACM0", "raw", "-echo"]) # arduino device id on ubuntu: /dev/ttyACM0
+dev = os.open("/dev/sdp1", os.O_RDWR)
+
 
 model = Yolov5Model("model/weights/best.pt")
 
@@ -20,7 +26,8 @@ while True:
             img = cv2.resize(img, (640, 480))
             pred = model.infer(img)
             if pred != None:
-                print(pred)
+                os.write(dev, (pred + "\n").encode())
+                print("sending %s" % pred)
         else:
             continue
     else:
